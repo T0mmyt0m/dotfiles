08f7a7fda811816d5f2e06701223064f2c66d1a3
T0mmyt0m <143659904+T0mmyt0m@users.noreply.github.com>
2025-02-19 16:05:28 -0500

    Update install_vim_config.sh

--- vim-config/install_vim_config.sh
+++ vim-config/install_vim_config.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-# Define paths
 VUNDLE_DIR=~/.vim/bundle/Vundle.vim
 DOTFILES_DIR=~/dotfiles/vim-config
 VIMRC_FILE=~/.vimrc
 
-# Ensure the dotfiles directory exists
 mkdir -p $DOTFILES_DIR
 
-# Copy .vimrc to the home directory
 cp $DOTFILES_DIR/.vimrc $VIMRC_FILE
 
-# Install Vundle if not exists
 if [ ! -d "$VUNDLE_DIR" ]; then
     echo "Installing Vundle..."
     git clone https://github.com/VundleVim/Vundle.vim.git $VUNDLE_DIR
 fi
 
-# Ensure .vimrc contains the required plugins
 if ! grep -q "Plugin 'tpope/vim-surround'" "$VIMRC_FILE"; then
     echo "Adding vim-surround plugin to .vimrc"
     cat <<EOL >> $VIMRC_FILE
